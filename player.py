cd25b9ee0d791c708d874ef2e8406c3d7ed79b22
Zollli <tothzoli1991s@gmail.com>
2019-05-20 15:03:14 +0200

    10  and pairs allin

--- player.py
+++ player.py
@@ -20,6 +20,12 @@ class Player:
                     return player['stack']
                 elif card1suit == card2suit:
                     return game_state["current_buy_in"] - player["bet"]
+                elif card1 in "AKQJ" and card2 == "10":
+                    return player['stack']
+                elif card2 in "AKQJ" and card1 == "10":
+                    return player['stack']
+                elif card1 == card2:
+                    return player['stack']
                 else:
                     return 0
         '''while len(comm_cards) == 0:
