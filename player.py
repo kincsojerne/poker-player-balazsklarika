9413b4f54ee95826bcd23e93103685c13a7e216f
Zollli <tothzoli1991s@gmail.com>
2019-05-20 16:10:39 +0200

    allin if we have pair

--- player.py
+++ player.py
@@ -14,8 +14,15 @@ class Player:
                 card2 = player["hole_cards"][1]["rank"]
                 card1suit = player["hole_cards"][0]["suit"]
                 card2suit = player["hole_cards"][1]["suit"]
-
                 us = player
+
+
+                for comm_card in comm_cards:
+                    if card1 == comm_card['rank'] or card2 == comm_card['rank']:
+                        return players['stack']
+
+
+
                 if (card1 in "AKQJ") and (card2 in "AKQJ"):
                     return game_state['current_buy_in'] - us['bet'] + game_state['minimum_raise']
                 elif card1 in "AKQJ" and card2 == "10":
@@ -26,6 +33,7 @@ class Player:
                     return game_state['current_buy_in'] - us['bet'] + game_state['minimum_raise']
                 else:
                     return 0
+
 
     def showdown(self, game_state):
         pass
