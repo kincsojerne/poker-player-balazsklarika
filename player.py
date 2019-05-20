d3d3bc4ef97a43025cc809cb96286630e3ea93a7
Zollli <tothzoli1991s@gmail.com>
2019-05-20 14:30:24 +0200

    refaktoring

--- player.py
+++ player.py
@@ -13,7 +13,7 @@ class Player:
                 card1 = player["hole_cards"][0]["rank"]
                 card2 = player["hole_cards"][1]["rank"]
                 us = player
-                if (card1 == "A" or card1 == "K" or card1 == "Q" or card1 == "J" or card1 == "10") and (card2 == "A" or card2 == "K" or card2 == "Q" or card2 == "J" or card2 == "10"):
+                if (card1 in "AKQJ") and (card2 in "AKQJ"):
                     return player['stack']
                 else:
                     return 0
