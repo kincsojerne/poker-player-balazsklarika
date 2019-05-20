96e711d1c914711aa72ea689c888a55ae5c19129
kincsojerne <voros.kincsojerne@gmail.com>
2019-05-20 11:58:25 +0200

    community cards pair check added

--- player.py
+++ player.py
@@ -1,9 +1,25 @@
 
 class Player:
-    VERSION = "0.1"
+    VERSION = "4.5"
 
     def betRequest(self, game_state):
-        return 1
+        players = game_state["players"]
+        comm_cards = game_state["community_cards"]
+        card1 = ""
+        card2 = ""
+        us = ""
+        for player in players:
+            if player["id"] == game_state["in_action"]:
+                card1 = player["hole_cards"][0]["rank"]
+                card2 = player["hole_cards"][1]["rank"]
+                us = player
+        for card in comm_cards:
+            if comm_cards[card]["rank"] == card1 or comm_cards[card]["rank"] == card:
+                return game_state["current_buy_in"] - (us["bet"] + game_state["minimum_raise"])
+        if card1 == card2:
+            return game_state["current_buy_in"] - (us["bet"] + game_state["minimum_raise"])
+        else:
+            return us["stack"]
 
     def showdown(self, game_state):
         pass
